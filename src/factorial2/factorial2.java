c7fc25e1b4d33982722ded4bdbe004f3ea190204
Oihana Mendez <oihanamv92@gmail.com>
2021-10-21 00:38:50 +0200

    first commit

---
+++ src/factorial2/factorial2.java
@@ -0,0 +1,22 @@
+package factorial2;
+
+public class factorial2
+{
+
+	   private static int numero;
+	public static void main(String[] args)
+       {
+               numero = 3;
+               System.out.println("El factorial de " + numero + " es " + calcularFactorial(numero));
+               numero = 5;
+               System.out.println("El factorial de " + numero + " es " + calcularFactorial(numero));
+       }
+       public static double calcularFactorial (double n) {
+               if (n == 0) {
+                       return 1;
+               } else {
+                       double resultado = n * calcularFactorial(n-1);
+                       return resultado;
+               }
+       }
+}
